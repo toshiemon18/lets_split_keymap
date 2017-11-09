3aa3cb221e3a6c8cb896547fb2bba67b0665d257
Toshiaki Seino <st12318@gmail.com>
2017-11-10 02:22:55 +0900

    Update

--- keymap.c
+++ keymap.c
@@ -24,22 +24,22 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Qwerty
+/* qwerty
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * | tab  |   q  |   w  |   e  |   r  |   t  |   y  |   u  |   i  |   o  |   p  | bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
+ * | ctrl |   a  |   s  |   d  |   f  |   g  |   h  |   j  |   k  |   l  |   ;  |enter |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  _   |
+ * |shift |   z  |   x  |   c  |   v  |   b  |   n  |   m  |  ,<  |  .>  |  /?  |  _   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  | Alt  | GUI  |Lower | Eisu |Space |Space | Kana | Left | Down | Up   | Right|
+ * |lower |ctrl  | alt  | gui  | eisu |space | Esc  | kana | gui  | alt  |  -   | "    |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,     KC_T,    KC_Y,    KC_U,     KC_I,     KC_O,    KC_P,    KC_BSPC, \
-  KC_LCTL, KC_A,    KC_S,    KC_D,   KC_F,     KC_G,    KC_H,    KC_J,     KC_K,     KC_L,    KC_SCLN, KC_ENT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,     KC_B,    KC_N,    KC_M,     KC_COMM,  KC_DOT,  KC_SLSH, KC_UNDS , \
-  KC_ESC,  KC_LALT, KC_LGUI, LOWER,  KC_LANG2, KC_SPC,  KC_SPC,  KC_LANG1, KC_LEFT,  KC_DOWN, KC_UP,   KC_RIGHT  \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,    KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_UNDS , \
+  LOWER,   KC_LCTL, KC_LALT, KC_LGUI, KC_LANG2, KC_SPC,  KC_ESC,  KC_LANG1, KC_RGUI, KC_RALT, KC_MINS, KC_QUOT  \
 ),
 
 /* Lower
@@ -50,14 +50,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * |------+------+------+------+------+------|------+------+------+------+------+------|
 * |      |   ~  |   `  |   |  |   \  |  -   |      |   [  |   ]  |   {  |   }  |      |
 * |------+------+------+------+------+------|------+------+------+------+------+------|
-* |Reset |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
+* |Reset |      |      |      | Left | Down | Up   |Right | Next | Vol- | Vol+ | Play |
 * `-----------------------------------------------------------------------------------'
 */
 [_LOWER] = KEYMAP( \
 _______, KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    _______, \
 _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, _______, \
 _______, KC_TILD, KC_GRV,  KC_PIPE, KC_BSLS,  KC_MINS, _______, KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR, _______, \
-RESET,   _______, _______, _______, _______,  _______, _______, _______,  KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
+RESET,   _______, _______, _______, KC_LEFT,  KC_DOWN, KC_UP,   KC_RIGHT, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
 
 /* Raise
