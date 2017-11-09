0991d2ce2fb30024c07b55f5cc4e5dbbb11c04e5
Toshiaki Seino <st12318@gmail.com>
2017-11-10 00:20:25 +0900

    Modified keymap

--- keymap.c
+++ keymap.c
@@ -11,13 +11,11 @@ extern keymap_config_t keymap_config;
 #define _QWERTY 0
 #define _LOWER  1
 #define _RAISE  2
-#define _ADJUST 3
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
-  ADJUST
 };
 
 // Fillers to make layering more clear
@@ -28,20 +26,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
+ * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  _   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  | GUI  | Eisu |Lower |Space |Space |Raise | Kana | GUI  | Alt  | Ctrl |
+ * | Esc  | Alt  | GUI  |Lower | Eisu |Space |Space | Kana | Left | Down | Up   | Right|
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
-  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_BSPC, \
-  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_ENT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_UNDS , \
-  KC_LCTL, KC_LALT, KC_LGUI, KC_LANG2, LOWER,  KC_SPC,  KC_SPC,  RAISE,   KC_LANG1, KC_RGUI, KC_RALT, KC_RCTL  \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,     KC_T,    KC_Y,    KC_U,     KC_I,     KC_O,    KC_P,    KC_BSPC, \
+  KC_LCTL, KC_A,    KC_S,    KC_D,   KC_F,     KC_G,    KC_H,    KC_J,     KC_K,     KC_L,    KC_SCLN, KC_ENT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,     KC_B,    KC_N,    KC_M,     KC_COMM,  KC_DOT,  KC_SLSH, KC_UNDS , \
+  KC_ESC,  KC_LALT, KC_LGUI, LOWER,  KC_LANG2, KC_SPC,  KC_SPC,  KC_LANG1, KC_LEFT,  KC_DOWN, KC_UP,   KC_RIGHT  \
 ),
 
 /* Lower
@@ -50,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * |------+------+------+------+------+------|------+------+------+------+------+------|
 * |      |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |      |
 * |------+------+------+------+------+------|------+------+------+------+------+------|
-* |      |   ~  |   `  |   |  |   \  |      |      |   [  |   ]  |   {  |   }  |      |
+* |      |   ~  |   `  |   |  |   \  |  -   |      |   [  |   ]  |   {  |   }  |      |
 * |------+------+------+------+------+------|------+------+------+------+------+------|
 * |Reset |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
 * `-----------------------------------------------------------------------------------'
@@ -58,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = KEYMAP( \
 _______, KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    _______, \
 _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, _______, \
-_______, KC_TILD, KC_GRV,  KC_PIPE, KC_BSLS,  _______, _______, KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR, _______, \
+_______, KC_TILD, KC_GRV,  KC_PIPE, KC_BSLS,  KC_MINS, _______, KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR, _______, \
 RESET,   _______, _______, _______, _______,  _______, _______, _______,  KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
 
@@ -79,27 +77,14 @@ KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  
 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
 RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET    \
-),
-
-/* Raise
-* ,-----------------------------------------, ,-----------------------------------------.
-* |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
-* |------+------+------+------+------+------| |------+------+------+------+------+------|
-* |      |      |      |      |      |      | |      |      |      |      |      |      |
-* |------+------+------+------+------+------| |------+------+------+------+------+------|
-* |      |      |      |      |      |      | |      |      |      |      |      |      |
-* |------+------+------+------+------+------| |------+------+------+------+------+------|
-* |Reset |      |      |      |      |      | |      |      |      |      |      |Reset |
-* `-----------------------------------------' `-----------------------------------------'
-*/
-
-[_ADJUST] = KEYMAP( \
-KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET    \
 )
 };
+
+#ifdef AUDIO_ENABLE
+float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
+float tone_dvorak[][2]     = SONG(DVORAK_SOUND);
+float tone_colemak[][2]    = SONG(COLEMAK_SOUND);
+#endif
 
 void persistent_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
@@ -107,6 +92,7 @@ void persistent_default_layer_set(uint16_t default_layer) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  bool return_value = false;
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
@@ -115,36 +101,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         #endif
         persistent_default_layer_set(1UL<<_QWERTY);
       }
-      return false;
       break;
+
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        /* update_tri_layer(_LOWER, _RAISE, _ADJUST); */
       } else {
         layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        /* update_tri_layer(_LOWER, _RAISE, _ADJUST); */
       }
-      return false;
       break;
+
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        /* update_tri_layer(_LOWER, _RAISE, _ADJUST); */
       } else {
         layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        /* update_tri_layer(_LOWER, _RAISE, _ADJUST); */
       }
-      return false;
       break;
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
-      }
-      return false;
-      break;
+
+    default:
+      return_value = true;
   }
-  return true;
+  return return_value;
 }
