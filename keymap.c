c6ebdd6560768cb4473cf3b82e2956ceb2cd2c41
Toshiaki Seino <st12318@gmail.com>
2017-12-06 14:36:12 +0900

    Solve very heavy layer switching

--- keymap.c
+++ keymap.c
@@ -11,11 +11,13 @@ extern keymap_config_t keymap_config;
 #define _QWERTY 0
 #define _LOWER  1
 #define _RAISE  2
+#define _ADJUST 16
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
+  ADJUST
 };
 
 // Fillers to make layering more clear
@@ -57,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 _______, KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    _______, \
 _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, _______, \
 _______, KC_TILD, KC_GRV,  KC_PIPE, KC_BSLS,  KC_MINS, _______, KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR, _______, \
-RESET,   _______, _______, _______, KC_LEFT,  KC_DOWN, KC_UP,   KC_RIGHT, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
+_______,   _______, _______, _______, KC_LEFT,  KC_DOWN, KC_UP,   KC_RIGHT, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
 
 /* Raise
@@ -73,6 +75,25 @@ RESET,   _______, _______, _______, KC_LEFT,  KC_DOWN, KC_UP,   KC_RIGHT, KC_MNX
 */
 
 [_RAISE] = KEYMAP( \
+KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
+_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET    \
+),
+
+/* Adjust
+* ,-----------------------------------------, ,-----------------------------------------.
+* |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+* |------+------+------+------+------+------| |------+------+------+------+------+------|
+* |      |      |      |      |      |      | |      |      |      |      |      |      |
+* |------+------+------+------+------+------| |------+------+------+------+------+------|
+* |      |      |      |      |      |      | |      |      |      |      |      |      |
+* |------+------+------+------+------+------| |------+------+------+------+------+------|
+* |Reset |      |      |      |      |      | |      |      |      |      |      |Reset |
+* `-----------------------------------------' `-----------------------------------------'
+*/
+
+[_ADJUST] = KEYMAP( \
 KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
@@ -106,20 +127,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
-        /* update_tri_layer(_LOWER, _RAISE, _ADJUST); */
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_LOWER);
-        /* update_tri_layer(_LOWER, _RAISE, _ADJUST); */
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       break;
 
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
-        /* update_tri_layer(_LOWER, _RAISE, _ADJUST); */
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_RAISE);
-        /* update_tri_layer(_LOWER, _RAISE, _ADJUST); */
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       break;
 
