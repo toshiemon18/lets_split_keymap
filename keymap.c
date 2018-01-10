e0a704a96e6e8cc1ad573d952aa5e9cfa8b66f8a
Toshiaki Seino <st12318@gmail.com>
2018-01-10 15:23:07 +0900

    Change keymap

--- keymap.c
+++ keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * |------+------+------+------+------+------|------+------+------+------+------+------|
 * | ctrl |   a  |   s  |   d  |   f  |   g  |   h  |   j  |   k  |   l  |  ;:  |enter |
 * |------+------+------+------+------+------|------+------+------+------+------+------|
-* |shift |   z  |   x  |   c  |   v  |   b  |   n  |   m  |  ,<  |  .>  |  /?  |  _   |
+* |shift |   z  |   x  |   c  |   v  |   b  |   n  |   m  |  ,<  |  .>  |  /?  |  -_   |
 * |------+------+------+------+------+------|------+------+------+------+------+------|
 * |lower |ctrl  | alt  | gui  | eisu |space | Esc  | kana | Left | Down | Up   |Right |
 * `-----------------------------------------------------------------------------------'
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = KEYMAP( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,    KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_UNDS , \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_MINS , \
   LOWER,   KC_LCTL, KC_LALT, KC_LGUI, KC_LANG2, KC_SPC,  KC_ESC,  KC_LANG1, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT  \
 ),
 
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = KEYMAP( \
 KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    _______, \
 KC_LCTL, KC_EXLM, KC_QUOT, KC_HASH, KC_DLR,   KC_PERC, KC_AMPR, KC_QUOT,  KC_LPRN, KC_RPRN, KC_LBRC, _______, \
-KC_LSFT, KC_TILD, KC_GRV,  KC_PIPE, KC_BSLS,  KC_MINS, KC_PLUS, KC_ASTR,  KC_CIRC, _______, KC_RBRC, _______, \
+KC_LSFT, KC_TILD, KC_GRV,  KC_PIPE, KC_BSLS,  KC_MINS, KC_PLUS, KC_ASTR,  KC_CIRC, KC_EQL,  KC_RBRC, _______, \
 _______, _______, _______, _______, _______,  _______, _______, _______,  KC_MNXT,  KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
 
